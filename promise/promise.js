5fa3b15fda3cf3bdced6f094960e68b4b78f4a13
longer790164074 <790164074@qq.com>
2021-07-06 23:27:57 +0800

    promise 静态方法

--- promise/promise.js
+++ promise/promise.js
@@ -28,8 +28,10 @@ class MyPromise {
             // 2. 传递结果【promiseResult】
             this.promiseResult = data
             // 3. 异步操作时，需要执行_callback里面的回调
-            this._callbacks.length > 0 && this._callbacks.forEach(t => {
-                t.onResolved(data)
+            setTimeout(() => {
+                this._callbacks.length > 0 && this._callbacks.forEach(t => {
+                    t.onResolved(data)
+                })
             })
         }
         // 创建reject函数
@@ -41,8 +43,10 @@ class MyPromise {
             // 2. 传递结果【promiseResult】
             this.promiseResult = data
             // 3. 异步操作时，需要执行_callback里面的回调
-            this._callbacks.length > 0 && this._callbacks.forEach(t => {
-                t.onRejected(data)
+            setTimeout(() => {
+                this._callbacks.length > 0 && this._callbacks.forEach(t => {
+                    t.onRejected(data)
+                })
             })
         }
         // 同步调用传入的函数
@@ -88,10 +92,10 @@ class MyPromise {
                 }
             }
             if (this.promiseState === this._FULFILLED) {
-                callBack(onResolved)
+                setTimeout(callBack(onResolved))
             }
             if (this.promiseState === this._REJECTED) {
-                callBack(onRejected)
+                setTimeout(callBack(onRejected))
             }
             // 判断pending状态，因为一般使用promise是异步调用的
             if (this.promiseState === this._PENDING) {
@@ -110,5 +114,81 @@ class MyPromise {
 
     catch(onRejected) {
         return this.then(undefined, onRejected)
+    }
+
+    // Promise.resolve
+    /**
+     * 如果入参为普通类型，则返回成功的Promise，如果是promise对象，则根据该对象的结果返回promise
+     */
+    static resolve(value) {
+        return new MyPromise((resolve, reject) => {
+            if (value instanceof MyPromise) {
+                // 是promise
+                value.then(res => resolve(res))
+                    .catch(err => reject(err))
+            } else {
+                // 非promise
+                resolve(value)
+            }
+        })
+    }
+
+    // Promise.reject
+    /**
+     * 如果入参为普通类型，则返回成功的Promise，如果是promise对象，则根据该对象的结果返回promise
+     */
+    static reject(reason) {
+        return new MyPromise((resolve, reject) => {
+            reject(reason)
+        })
+    }
+
+    // Promise.all
+    /**
+     * 用于将多个promise实例，包装成一个promise实例
+     * 1. 如果iterator对象的成员不是promise，则会调用Promise.resolve()转换为promise
+     * 2. 只有所有成员的状态都为fulfilled,返回的promise实例才会为fulfilled
+     * 3. 只要有一个成员是 rejected,则返回的就是rejected状态的promise
+     */
+    static all(promises) {
+        return new MyPromise((resolve, reject) => {
+            // 判断是否为iterator对象
+            // 如果有非iterator对象成员，则开始Promise.resolve()
+            // 开始遍历执行，如果有rejected，则调用 rejectd，反之调用resolved
+            // 此时假设传入的是数组，数组中所有成员都是promise对象
+            let count = 0 // 记录成功次数
+            let resolvedArr = [] //返回成功结果
+            for (let i = 0; i < promises.length; i++) {
+                promises[i].then(res => {
+                    // 成功了
+                    count++
+                    // 将结果存入数组中，并且保证顺序(使用索引存储)
+                    resolvedArr[i] = res
+                    if (count === promises.length) {
+                        // 修改状态并且返回所有结果的数组
+                        resolve(resolvedArr)
+                    }
+                }).catch(err => {
+                    // 失败了
+                    reject(err)
+                })
+            }
+        })
+    }
+
+    // Promise.race
+    /**
+     * 用于将多个promise实例，包装成一个promise实例
+     * 1. 如果iterator对象的成员不是promise，则会调用Promise.resolve()转换为promise
+     * 2. 将那个率先改变状态的promise成员的值，作为返回的promise的值
+     */
+    static race(promises) {
+        return new MyPromise((resolve, reject) => {
+            for (let i = 0; i < promises.length; i++) {
+                // 哪个快就执行哪个，并且将结果返回
+                promises[i].then(res => resolve(res))
+                    .catch(err => reject(err))
+            }
+        })
     }
 }
