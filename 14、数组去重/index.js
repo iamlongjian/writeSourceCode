d28aed05aa1450bb97b110083cc71436fdc65b40
longer790164074 <790164074@qq.com>
2021-07-19 00:03:33 +0800

    update

--- 14、数组去重/index.js
+++ 14、数组去重/index.js
@@ -1,4 +1,5 @@
 // 编写一个去重函数，实现数组的去重
+const arr = [{ a: 1 }, 2, { a: 1 }, 1, 4, 53, 3, 5, 4]
 // 实现一：利用Set
 // function unique(arr) {
 //     return [...new Set(arr)]
@@ -15,7 +16,14 @@
 // }
 
 // 实现三：forEach + indexOf（双重循环，性能差）
+// function unique(arr) {
+//     const res = []
+//     arr.forEach(item => {
+//         if (res.indexOf(item) === -1) {
+//             res.push(item)
+//         }
+//     });
+//     return res
+// }
 
-
-const arr = [1, 2, 2, 1, 4, 53, 3, 5, 4]
 console.log(unique(arr));
